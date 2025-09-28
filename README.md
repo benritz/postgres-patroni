489d857d0905514d3268d9a74883c2db6b702844
benritz <23510442+benritz@users.noreply.github.com>
2025-09-28 21:59:46 +0100

    cleanup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # postgres-patroni
 Postgres run by Patroni for HA
+
+## Environment variables
+
+The following variables must be set before running docker compose up.
+
+- CLUSTER - the name of the cluster
+
+- BACKUP_REGION
+- BACKUP_PATH
+- BACKUP_ACCESS_KEY
+- BACKUP_SECRET_KEY
--- etcd/container-entrypoint.sh
+++ etcd/container-entrypoint.sh
@@ -6,7 +6,11 @@ if [ -z "$ETCD_NAME" ]; then
   export ETCD_NAME=${HOSTNAME}
 fi
 
+export ETCD_DATA_DIR=/var/etcd-data
 export ETCD_INITIAL_ADVERTISE_PEER_URLS=http://${HOSTNAME}:2380
 export ETCD_ADVERTISE_CLIENT_URLS=http://${HOSTNAME}:2379
+export ETCD_LISTEN_PEER_URLS=http://0.0.0.0:2380
+export ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379
+export ETCD_INITIAL_CLUSTER_STATE=new
 
 etcd
